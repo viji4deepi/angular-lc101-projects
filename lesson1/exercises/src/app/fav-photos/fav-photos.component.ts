1ccc7129dbd135ca42f394079ef5ab6782f96680
viji <viji4deepi@gmail.com>
2022-03-20 22:57:51 -0500

    first lessons

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+ //photosTitle = 'Photo/Image Section Title Here';
+ photosTitle = 'Random Images';
+  image1 = 'https://upload.wikimedia.org/wikipedia/commons/b/ba/ArcheryGermanyEarly1980s-2.jpg';
+  image2 = 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/HONDA_ASIMO.jpg/220px-HONDA_ASIMO.jpg';
+  image3 = 'https://www.launchcode.org/assets/homepage/desktop-computer-133c1d5afdb40b0fa1d6bf8cc3bece17d85b685ac21dd0da068433aa5ef885a2.png';
+  //https://www.launchcode.org/assets/homepage/desktop-computer-133c1d5afdb40b0fa1d6bf8cc3bece17d85b685ac21dd0da068433aa5ef885a2.png
 
   constructor() { }
 
